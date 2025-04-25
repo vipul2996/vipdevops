65ac0dc3b9d46bc2a3f634b1664c62ce12d0904d
vipul depolkar <Vdepolkar@nba.com>
2025-04-25 15:27:24 +0530

    added backend

--- local.tf
+++ local.tf
@@ -1,23 +1,21 @@
 terraform {
   backend "s3" {
-    bucket         = "invstatebackup"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
-    
+    bucket = "invstatebackup"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+
   }
 }
 
 provider "aws" {
-    # shared_config_files      = ["C:\\Users\\vdepolkar\\.aws\\config"]
-    # shared_credentials_files = ["C:\\Users\\vdepolkar\\.aws\\credentials"] #No need as this will go in github actions
-  region                   = "us-east-1"
+  region = "us-east-1"
 }
 
 terraform {
-    required_providers {
-        aws = {
-        source  = "hashicorp/aws"
-        version = "~> 5.0"
-        }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
+  }
 }
