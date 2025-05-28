9f193f790467613f893814462bac270dd5fd2bf8
vipul2996 <60309554+vipul2996@users.noreply.github.com>
2025-05-28 15:18:33 +0530

    Update local.tf

--- local.tf
+++ local.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "invstatebackup"
     key            = "terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "invstatelock"
+    dynamodb_table = "inv-statelock"
 
   }
 }
