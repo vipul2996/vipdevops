500eb99bf90338814172f3b79959136bcf58d2b5
vipul2996 <60309554+vipul2996@users.noreply.github.com>
2025-05-28 13:23:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,3 +6,12 @@ resource "aws_s3_bucket" "example" {
     Environment = "Dev"
   }
 }
+
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "inv-vpc-001"
+  }
+}
