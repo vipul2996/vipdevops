dd4faf12a1172d0535df9e1dd5269aeb4744ccb0
vipul2996 <60309554+vipul2996@users.noreply.github.com>
2025-05-29 16:14:59 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -15,3 +15,11 @@ resource "aws_vpc" "main" {
     Name = "inv-vpc-001"
   }
 }
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Main"
+  }
+}
